226fae7dffa46372741bb6011fc7f954adbff651
Shubhankar thakur <53370662+saims0n@users.noreply.github.com>
2021-02-03 17:47:39 +0530

    Add files via upload

---
+++ bob/notes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "-= Notes =-"
+echo "Harry Potter is my faviorite"
+echo "Are you the real me?"
+echo "Right, I'm ordering pizza this is going nowhere"
+echo "People just don't get me"
+echo "Ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <sea santy here>"
+echo "Cucumber"
+echo "Rest now your eyes are sleepy"
+echo "Are you gonna stop reading this yet?"
+echo "Time to fix the server"
+echo "Everyone is annoying"
+echo "Sticky notes gotta buy em"
